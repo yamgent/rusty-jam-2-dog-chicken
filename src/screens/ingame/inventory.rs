cb8fa1b5bbc8df6301923c6d50db6c4291087a3a
Tan Wang Leng <tanwangleng@outlook.com>
2022-06-21 23:49:49 +0800

    Clean up inventory.rs

--- src/screens/ingame/inventory.rs
+++ src/screens/ingame/inventory.rs
@@ -5,15 +5,20 @@ use crate::{
     ui,
 };
 
-const PAGE_COL_COUNT: u32 = 5;
-const PAGE_ROW_COUNT: u32 = 3;
-const PAGE_COUNT: u32 = PAGE_COL_COUNT * PAGE_ROW_COUNT;
+const PAGE_COL_COUNT: usize = 5;
+const PAGE_ROW_COUNT: usize = 3;
+const PAGE_COUNT: usize = PAGE_COL_COUNT * PAGE_ROW_COUNT;
+
+const PAGE_ITEM_START_X: u32 = 12;
+const PAGE_ITEM_START_Y: u32 = 52;
+const PAGE_ITEM_GAP: u32 = 4;
 
 pub struct Inventory {
     found: Vec<Item>,
-    select_idx: u32,
+    select_idx: usize,
 }
 
+#[derive(PartialEq, Debug)]
 pub enum AddResult {
     Success,
     AlreadyFound,
@@ -28,18 +33,18 @@ impl Inventory {
     }
 
     pub fn selected_item(&self) -> Item {
-        self.found[self.select_idx as usize]
+        self.found[self.select_idx]
     }
 
-    fn current_page(&self) -> u32 {
+    fn current_page(&self) -> usize {
         self.select_idx / PAGE_COUNT
     }
 
-    fn total_pages(&self) -> u32 {
-        if self.found.len() == 0 {
+    fn total_pages(&self) -> usize {
+        if self.found.is_empty() {
             0
         } else {
-            (((self.found.len() as u32) - 1) / PAGE_COUNT) + 1
+            ((self.found.len() - 1) / PAGE_COUNT) + 1
         }
     }
 
@@ -54,32 +59,36 @@ impl Inventory {
 
     pub fn update(&mut self, input: &Input) {
         if input.pressed(BUTTON_RIGHT) {
+            let new_idx = self.select_idx + 1;
+
             let old_row = self.select_idx / PAGE_COL_COUNT;
             let new_row = (self.select_idx + 1) / PAGE_COL_COUNT;
 
             if old_row == new_row {
-                if self.select_idx + 1 < self.found.len() as u32 {
-                    self.select_idx += 1;
+                if new_idx < self.found.len() {
+                    self.select_idx = new_idx;
                 }
             } else {
-                let new_idx = self.select_idx + PAGE_COUNT - (PAGE_COL_COUNT - 1);
                 if self.current_page() + 1 < self.total_pages() {
-                    if new_idx < self.found.len() as u32 {
+                    let new_idx = self.select_idx + PAGE_COUNT - (PAGE_COL_COUNT - 1);
+
+                    if new_idx < self.found.len() {
                         self.select_idx = new_idx;
                     } else {
+                        // just go to the next page and select the first item
                         self.select_idx = (self.current_page() + 1) * PAGE_COUNT;
                     }
                 }
             }
         } else if input.pressed(BUTTON_LEFT) {
             if self.select_idx > 0 {
+                let new_idx = self.select_idx - 1;
+
                 let old_row = self.select_idx / PAGE_COL_COUNT;
-                let new_row = (self.select_idx - 1) / PAGE_COL_COUNT;
+                let new_row = new_idx / PAGE_COL_COUNT;
 
                 if old_row == new_row {
-                    if self.select_idx != 0 {
-                        self.select_idx -= 1;
-                    }
+                    self.select_idx = new_idx;
                 } else {
                     if self.current_page() != 0 {
                         self.select_idx -= PAGE_COUNT - (PAGE_COL_COUNT - 1);
@@ -88,20 +97,22 @@ impl Inventory {
             }
         } else if input.pressed(BUTTON_UP) {
             if self.select_idx >= PAGE_COL_COUNT {
+                let new_idx = self.select_idx - PAGE_COL_COUNT;
                 let old_page = self.current_page();
-                let new_page = (self.select_idx - PAGE_COL_COUNT) / PAGE_COUNT;
+                let new_page = new_idx / PAGE_COUNT;
 
                 if old_page == new_page {
-                    self.select_idx -= PAGE_COL_COUNT;
+                    self.select_idx = new_idx;
                 }
             }
         } else if input.pressed(BUTTON_DOWN) {
-            if self.select_idx + PAGE_COL_COUNT < self.found.len() as u32 {
+            let new_idx = self.select_idx + PAGE_COL_COUNT;
+            if new_idx < self.found.len() {
                 let old_page = self.current_page();
-                let new_page = (self.select_idx + PAGE_COL_COUNT) / PAGE_COUNT;
+                let new_page = new_idx / PAGE_COUNT;
 
                 if old_page == new_page {
-                    self.select_idx += PAGE_COL_COUNT;
+                    self.select_idx = new_idx;
                 }
             }
         }
@@ -110,25 +121,24 @@ impl Inventory {
     pub fn draw(&self) {
         let current_page = self.current_page();
         let total_pages = self.total_pages();
-        // items
-        let start_x = 12u32;
-        let start_y = 52u32;
-        let item_gap = 4u32;
 
+        // items
+        let first_item_idx = current_page * PAGE_COUNT;
         self.found
             .iter()
-            .skip((current_page * PAGE_COUNT) as usize)
-            .take(PAGE_COUNT as usize)
+            .skip(first_item_idx)
+            .take(PAGE_COUNT)
             .enumerate()
-            .for_each(|(idx, item)| {
-                let x = (start_x + ((idx as u32 % PAGE_COL_COUNT) * (ui::ITEM_WIDTH_PX + item_gap)))
+            .for_each(|(rel_idx, item)| {
+                let x = (PAGE_ITEM_START_X
+                    + ((rel_idx % PAGE_COL_COUNT) as u32 * (ui::ITEM_WIDTH_PX + PAGE_ITEM_GAP)))
                     as i32;
-                let y = (start_y
-                    + ((idx as u32 / PAGE_COL_COUNT) * (ui::ITEM_HEIGHT_PX + item_gap)))
+                let y = (PAGE_ITEM_START_Y
+                    + ((rel_idx / PAGE_COL_COUNT) as u32 * (ui::ITEM_HEIGHT_PX + PAGE_ITEM_GAP)))
                     as i32;
                 ui::draw_item(*item, x, y);
 
-                if (idx as u32 + (current_page * PAGE_COUNT)) == self.select_idx {
+                if first_item_idx + rel_idx == self.select_idx {
                     unsafe { *DRAW_COLORS = 0x40 };
                     rect(x - 2, y - 2, ui::ITEM_WIDTH_PX + 4, ui::ITEM_HEIGHT_PX + 4);
                 }
@@ -138,23 +148,87 @@ impl Inventory {
         if self.total_pages() > 1 {
             if self.current_page() < self.total_pages() - 1 {
                 ui::draw_text(">\n>\n>\n>\n>\n>", 160 - 8, 70);
+
                 unsafe { *DRAW_COLORS = 0x20 }
                 rect(160 - 8 - 1, 70 - 2, 7 + 2, 49 + 2);
             }
 
             if self.current_page() > 0 {
                 ui::draw_text("<\n<\n<\n<\n<\n<", 1, 70);
+
                 unsafe { *DRAW_COLORS = 0x20 }
                 rect(0, 70 - 2, 7 + 2, 49 + 2);
             }
         }
 
         // bottom text
-        ui::draw_text_bottom_left(self.found[self.select_idx as usize].name(), 2, 160 - 2);
+        ui::draw_text_bottom_left(self.found[self.select_idx].name(), 2, 160 - 2);
         ui::draw_text_bottom_right(
             format!("Page\n{}/{}", current_page + 1, total_pages),
             160 - 2,
             160 - 2,
         );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter;
+
+    #[test]
+    fn test_selected_item() {
+        let mut inv = Inventory {
+            found: vec![Item::Dog, Item::Chicken, Item::DogChicken],
+            select_idx: 0,
+        };
+
+        assert_eq!(inv.selected_item(), Item::Dog);
+
+        inv.select_idx = 1;
+        assert_eq!(inv.selected_item(), Item::Chicken);
+
+        inv.select_idx = 2;
+        assert_eq!(inv.selected_item(), Item::DogChicken);
+    }
+
+    #[test]
+    fn test_current_page() {
+        let mut inv = Inventory::new();
+        inv.select_idx = 0;
+        assert_eq!(0, inv.current_page());
+
+        inv.select_idx = PAGE_COUNT - 1;
+        assert_eq!(0, inv.current_page());
+
+        inv.select_idx = PAGE_COUNT;
+        assert_eq!(1, inv.current_page());
+    }
+
+    #[test]
+    fn test_total_pages() {
+        let mut inv = Inventory::new();
+        inv.found.clear();
+        assert_eq!(0, inv.total_pages());
+
+        inv.found = vec![Item::Dog];
+        assert_eq!(1, inv.total_pages());
+
+        inv.found = iter::repeat(Item::Dog).take(PAGE_COUNT).collect::<Vec<_>>();
+        assert_eq!(1, inv.total_pages());
+
+        inv.found.push(Item::Dog);
+        assert_eq!(2, inv.total_pages());
+    }
+
+    #[test]
+    fn test_add_found() {
+        let mut inv = Inventory::new();
+        inv.found.clear();
+
+        assert_eq!(inv.add_found(Item::Dog), AddResult::Success);
+        assert_eq!(inv.found, vec![Item::Dog]);
+        assert_eq!(inv.add_found(Item::Dog), AddResult::AlreadyFound);
+        assert_eq!(inv.found, vec![Item::Dog]);
     }
 }
