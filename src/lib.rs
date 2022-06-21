c98918e36bdf78036737921208586be9a67adc60
Tan Wang Leng <tanwangleng@outlook.com>
2022-06-21 11:08:56 +0800

    Add logic for drawing a single object

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,28 @@ const SMILEY: [u8; 8] = [
     0b11000011,
 ];
 
+const OBJECT_ALTAS_COL_COUNT: u32 = 8;
+const SINGLE_OBJ_PIXELS: u32 = assets::objects::OBJECTS_PNG_WIDTH / OBJECT_ALTAS_COL_COUNT;
+
+fn draw_object(idx: u32, x: i32, y: i32) {
+    let src_x = (idx % OBJECT_ALTAS_COL_COUNT) * SINGLE_OBJ_PIXELS;
+    let src_y = (idx / OBJECT_ALTAS_COL_COUNT) * SINGLE_OBJ_PIXELS;
+    blit_sub(
+        &assets::objects::OBJECTS_PNG,
+        x,
+        y,
+        SINGLE_OBJ_PIXELS,
+        SINGLE_OBJ_PIXELS,
+        src_x,
+        src_y,
+        assets::objects::OBJECTS_PNG_WIDTH,
+        assets::objects::OBJECTS_PNG_FLAGS,
+    );
+}
+
 #[no_mangle]
 fn update() {
-    unsafe { *DRAW_COLORS = 0x0222 }
+    unsafe { *DRAW_COLORS = 0x0123 }
     text("Hello from Rust!", 10, 10);
 
     let gamepad = unsafe { *GAMEPAD1 };
@@ -27,13 +46,6 @@ fn update() {
     }
 
     blit(&SMILEY, 76, 76, 8, 8, BLIT_1BPP);
-    blit(
-        &assets::objects::OBJECTS_PNG,
-        0,
-        0,
-        assets::objects::OBJECTS_PNG_WIDTH,
-        assets::objects::OBJECTS_PNG_HEIGHT,
-        assets::objects::OBJECTS_PNG_FLAGS,
-    );
+    draw_object(0, 0, 0);
     text("Press X to blink", 16, 90);
 }
