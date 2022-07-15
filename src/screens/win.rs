412089780e1f86c53af3420bffbebed7ffb96977
Tan Wang Leng <tanwangleng@outlook.com>
2022-07-15 22:17:44 +0800

    Allow game restart from win screen

--- src/screens/win.rs
+++ src/screens/win.rs
@@ -1,5 +1,10 @@
-use super::Screen;
-use crate::{input::Input, item::Item, ui};
+use super::{ingame::IngameScreen, Screen};
+use crate::{
+    input::Input,
+    item::Item,
+    ui,
+    wasm4::{BUTTON_1, BUTTON_2},
+};
 use strum::IntoEnumIterator;
 
 pub struct WinScreen {
@@ -17,14 +22,17 @@ impl WinScreen {
 }
 
 impl Screen for WinScreen {
-    fn update(&mut self, _input: &Input) -> Option<Box<dyn Screen + Send>> {
+    fn update(&mut self, input: &Input) -> Option<Box<dyn Screen + Send>> {
+        if input.pressed(BUTTON_1) && input.pressed(BUTTON_2) {
+            return Some(Box::new(IngameScreen::new()));
+        }
         None
     }
 
     fn draw(&self) {
         ui::draw_text_top_center("YOU FOUND\nTHE DOGCHICKEN!", 80, 32);
         ui::draw_item(Item::DogChicken, 80 - (ui::ITEM_WIDTH_PX as i32 / 2), 64);
-        ui::draw_text_top_center("Thank you for\nplaying", 80, 110);
+        ui::draw_text_top_center("Thank you for\nplaying\n(Z+X to restart)", 80, 110);
         ui::draw_text_bottom_left(
             format!(
                 "<{}/{} found>",
